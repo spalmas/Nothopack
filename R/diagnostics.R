f99a4a3b8bbf577abcfe83a747571af7d7c1fb45
spalmas <sebaspa@gmail.com>
2016-05-16 10:01:44 -0400

    Added Dominant Height data

---
+++ R/diagnostics.R
@@ -0,0 +1,39 @@
+#' Diagnostic plots for regression fitted and predicted values
+#'
+#' @param obs Array of observed values.
+#' @param pred Array of predicted values.
+#' @return Four different diagnostic plots
+#' @examples
+#' library(Nothopack)
+#' fit <- lm(sr ~ ., data = LifeCycleSavings)
+#' diagnostics(LifeCycleSavings$sr, predict(fit))
+#'
+
+diagnostics<-function(obs,pred){
+
+  resid <-  obs - pred
+  par(mfrow=c(2,2))
+
+  # Plot 1 -Predicted vs observed
+  plot(y=resid,x=pred, main='Fitted value plot',
+       ylab="Residual", xlab="Fitted value")
+  abline(h=0,col="blue")
+
+  # Plot 2 - Index Plot
+  index<-seq(1,length(resid))
+  plot(y=resid,x=index, main='Index plot', ylab="Residual", xlab="Index")
+  abline(h=0,col="blue")
+
+  # Plot 3 - A histogram with normal distribution for studentized residuals
+  hist(resid, freq=FALSE, main='Residuals distribution',
+       ylab='Frequency',xlab='Residual')
+  xfit<-seq(min(resid)*1.1,max(resid)*1.1,length=80)
+  yfit<-dnorm(xfit,mean=mean(resid),sd=sd(resid))
+  lines(xfit, yfit,col="blue",lwd=1)
+
+  # Plot 4 - QQ-Plot
+  qqnorm(resid, main='Normal Q-Q plot', ylab='Ordered residual',xlab='Normal quantile')
+  qqline(resid,col='blue')
+
+  par(mfrow=c(1,1))
+}
---
+++ R/hd_coef.R
@@ -0,0 +1,14 @@
+#' Dominant Height Coefficients for Nothofagus species
+#'
+#' Dominant height ecuation parameters:
+#' HD = 0.3 + a [1 – {1 – (IS / a) c } ((E + 0.5) / 18)] 1/c
+#' c = b0 + b1 * IS
+#'
+#' @docType data
+#'
+#' @usage data(hd_coef)
+#'
+#' @keywords datasets
+#'
+#' @references Gezan and Ortega. 2001. Desarrollo de un simulador de rendimiento para renovales de Roble, Rauli y Coigue.
+"hd_coef"
