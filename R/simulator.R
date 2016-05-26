532fb3e6cb12ed56d585daecb91052967cbf3459
spalmas <sebaspa@gmail.com>
2016-05-26 11:18:29 -0400

    modified example for Simulator

--- R/simulator.R
+++ R/simulator.R
@@ -6,13 +6,15 @@
 #' @param zone zone of the stand
 #' @param age initial age of the stand
 #' @param sy simuation years
+#' @param Nmodel Number of model for NHA estimation (1 is original function, 2 for new function)
+#' @param BAmodel 1 for non linear and 2 for linear coefficient estimates
 #'
 #' @author
 #' S.Gezan, S.Palmas and P.Moreno
 #'
 #' @examples
 #' stand <- stand_randomizer()
-#' simulator(stand, zone = 1, age = 15)
+#' simulator(stand, zone = 1, age = 15, EDOM0 = 20)
 
 simulator <- function(stand, zone, EDOM0, area = 10000, sy = 30, Nmodel = 1, ABmodel = 1,  ...){
 
@@ -26,14 +28,14 @@ simulator <- function(stand, zone, EDOM0, area = 10000, sy = 30, Nmodel = 1, ABm
   y <- 0   #initial year
 
   #create a table to store results
-  results <- data.frame (y = y, N0 = prodal$NHA, AB0 = AB0, DC0 = DC0)
+  results <- data.frame (y = y, N0 = N0, AB0 = AB0, DC0 = DC0)
 
   for (y in 1:sy){
     N1<-Nmodule(N0=N0,QD0=DC0,model=Nmodel)
     AB1<-BAmodule(EDOM0=EDOM0,HDOM0=HDOM0,N0=N1,BA0=AB0,model=ABmodel,projection=TRUE)$BA1
     DC1<-remaining_calc_A(AB = AB1, NHA=N1)
 
-    results <- rbind(results, c(y, N1, AB1, DC0))
+    results <- rbind(results, c(y, N1, AB1, DC0))  #should be in the same order as dataframe above!
 
     #Variable replacement
     N0 <- N1
