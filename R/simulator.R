6b57b48f93e7724d0f70b908673297844ee1c94c
spalmas <sebaspa@gmail.com>
2016-05-31 11:51:26 -0400

    DC0 to DC1

--- R/simulator.R
+++ R/simulator.R
@@ -29,18 +29,20 @@ simulator <- function(stand, zone, ED0, area = 10000, sy = 30, Nmodel = 1, ABmod
   y <- 0   #initial year
 
   #create a table to store results
-  results <- data.frame (y = y, N0 = N0, AB0 = AB0, DC0 = DC0)
+  results <- data.frame (y = y, N0 = N0, AB0 = AB0, DC0 = DC0, HD = HD0)
   for (y in 1:sy){
     N1<-Nmodule(N0=N0,QD0=DC0,model=Nmodel)
     AB1<-BAmodule(ED0=ED0,HD0=HD0,N0=N1,BA0=AB0,model=ABmodel,projection=TRUE)$BA1
     DC1<-get_stand(BA = AB1, N=N1)
+    HD1<-HD0  #should be replaced with equations
 
-    results <- rbind(results, c(y, N1, AB1, DC0))  #should be in the same order as dataframe above!
+    results <- rbind(results, c(y, N1, AB1, DC1, HD1))  #should be in the same order as dataframe above!
 
     #Variable replacement
     N0 <- N1
     DC0 <- DC1
     AB0 <- AB1
+    HD <- HD1
   }
 
 
