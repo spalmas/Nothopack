487450e1c63dab533a475fa242cb800a6ed3706f
Sebastian Palmas <sebaspa@gmail.com>
2016-06-17 13:34:35 +0100

    standarizing names

--- R/simulator_validation.R
+++ R/simulator_validation.R
@@ -10,7 +10,7 @@ ensayos.data <- ensayos.data[!ensayos.data$dom_sp ==4,]
 
 #adding columns to right of the ensayos dataset
 ensayos.data[,(ncol(ensayos.data) + 1):(ncol(ensayos.data) + 6)] <- NA
-colnames(ensayos.data)[(ncol(ensayos.data) -5):(ncol(ensayos.data))] <- c('y', 'N1_pred', 'BA1_pred', 'QD1_pred', 'HD1_pred', 'SI_pred')
+colnames(ensayos.data)[(ncol(ensayos.data) -5):(ncol(ensayos.data))] <- c('y', 'NHA1_pred', 'BA1_pred', 'QD1_pred', 'HD1_pred', 'SI_pred')
 
 #simulation of each row of the validation dataset r=3
 for (r in 1:nrow(ensayos.data)){
@@ -33,14 +33,15 @@ plot(ensayos.data$BA1, ensayos.data$BA1_pred)
 abline(a=0, b=1)
 
 ensayos.data['BAdif'] <- ensayos.data$BA1 - ensayos.data$BA1_pred
+hist(ensayos.data$BAdif)
 plot(ensayos.data$Delta.ANHO, ensayos.data$BAdif)
 abline(a=0, b=0)
 
 #predicted vs observed NHA
-plot(ensayos.data$NHA1, ensayos.data$N1_pred)
+plot(ensayos.data$NHA1, ensayos.data$NHA1_pred)
 abline(a=0, b=1)
 
-ensayos.data['NHAdif'] <- ensayos.data$NHA1 - ensayos.data$N1_pred
+ensayos.data['NHAdif'] <- ensayos.data$NHA1 - ensayos.data$NHA1_pred
 plot(ensayos.data$Delta.ANHO, ensayos.data$NHAdif)
 abline(a=0, b=0)
 
