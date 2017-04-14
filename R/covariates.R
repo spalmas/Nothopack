1151fc5bffb27ef618e21d4162b159d03f721673
pmorenomeynard <pmorenomeynard@gmail.com>
2017-04-13 22:54:51 -0400

    recovering lost information

--- R/covariates.R
+++ R/covariates.R
@@ -22,4 +22,40 @@
 ## Code for obtaining covariates from ingrowth DBH models
 ## Require plot with id tree, area, sp, DBH and zone
 
-
+covariates<-function(ID,area,sp,DBH,ZONA,SS){
+  #require(dplyr)
+  N <- length(ID)#Getting the number of trees in the plot
+  area.plot <- unique(area)   #area of the measurement
+  NHA <- N * 10000/area.plot #Getting the number of trees in the stand by hectare
+  BA =  sum(pi * (DBH/2/100)^2, na.rm = TRUE) * 10000/area.plot#total basal area by hectare
+  QD <-  (100*((4/pi)*(BA/NHA))^0.5)#total quadratic diameter,
+  SDI<-NHA*((25.4/QD)^(-1.4112))#Stand density index
+  SPZONA<-sp*10+ZONA
+  ###BAL & BALc Calculation
+  ba<-pi*((DBH)^2)/40000
+  bac<-0
+  for (fila in (1:(N))) {
+    bac[fila] <- if (sp[fila] != 4) {ba[fila]}else {0}
+  }
+  Temp.data<-data.frame(ID,DBH,ba,bac,SP,ZONA,SPZONA,NHA,BA,QD,SDI,SS)
+  Temp.data1<-arrange(Temp.data, desc(ba))
+  Acum=0;Acumc=0
+  Temp.data1$BAL[1]=0
+  Temp.data1$BALc[1]=0
+  for (i in (2:nrow(Temp.data1))) {
+    Acum <- Acum + (Temp.data1$ba[i-1]*10000/area.plot)
+    Temp.data1$BAL[i] <- Acum
+    if(Temp.data1$bac[i]!=0){
+      Acumc <- Acumc + (Temp.data1$bac[i-1]*10000/area.plot)
+      Temp.data1$BALc[i] <- Acumc
+    }
+    else {Temp.data1$BALc[i]=NA}
+  }
+  newBALc<-min(Temp.data1$BALc,na.rm = TRUE)
+  for (i in (1:nrow(Temp.data1))){
+    if (is.na(Temp.data1$BALc[i])){Temp.data1$BALc[i]=newBALc}
+  }
+  Temp.data1$PScal<-Temp.data1$BAL/Temp.data1$BA # Sociologic status calculated by competence and stocking
+  if (is.na(Temp.data1$SS)){
+    Temp.data1$SS<-Social_Status(Temp.data1$PScal)
+}
