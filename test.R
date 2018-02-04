a8cda3b1ebce782033f5a689664e03b59add94ac
pmorenomeynard <pmorenomeynard@gmail.com>
2018-02-04 19:16:56 -0300

    documents

--- test.R
+++ test.R
@@ -1,12 +1,12 @@
 # tests
 
-plot<- read.csv(file= 'C:/MASTER UFL/research/Gezan/Modelling/INFO Nothofagus 2/Research Palmas/UACH_P87_M1.csv')
+plot<- read.csv(file= 'C:/MASTER UFL/research/Gezan/Modelling/INFO Nothofagus 2/Research Palmas/UACH_P41_M1.csv')
 head(plot)
-tree<-inputmodule(type='tree',zone=3,AD=52.31,HD=42.40,SI=NA,area=500,AF=58.31,tree.list=plot,V_model=1)
+tree<-inputmodule(type='tree',zone=2,AD=51.85,HD=30.0,SI=NA,area=500,AF=57.85,tree.list=plot,V_model=1)
 head(tree$tree.list)
 core.tree<-core_module(input=tree$input)
 core.tree$sp.table
-core.tree$type<-"stand"
+core.tree$type<-"tree"
 stand<-stand_simulator(core.stand = core.tree)
 results.stand<-core_module(input = stand$input)
 results.stand$sp.table
